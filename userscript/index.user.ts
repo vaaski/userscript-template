17a1f6766cc6af4344281983d2de2ab27f05209a
vaaski <admin@vaa.ski>
2026-07-29 00:10:51 +0200

    menu example

--- userscript/index.user.ts
+++ userscript/index.user.ts
@@ -1,6 +1,21 @@
 import { loadStyle } from "~~/build/styles" with { type: "macro" }
 import { injectStyle } from "~~/userscript/util/inject-style"
 
-injectStyle(loadStyle("styles/main.css"))
+let removeInjection: (() => void) | undefined = injectStyle(loadStyle("styles/main.css"))
 
-GM.registerMenuCommand("Text", () => console.log("text"))
+GM.registerMenuCommand(
+	"Toggle CSS injection",
+	() => {
+		if (removeInjection) {
+			removeInjection()
+			removeInjection = undefined
+		} else {
+			removeInjection = injectStyle(loadStyle("styles/main.css"))
+		}
+	},
+	{
+		icon: "https://www.google.com/s2/favicons?sz=64&domain=google.com",
+		autoClose: false,
+		title: "Example menu command",
+	},
+)
